180ff5188d0a1ce25766fa171789791e44fe306a
kwang kim <bkwangkim@gmail.com>
2022-06-03 13:44:37 -0400

    fix

--- python/download-trade.py
+++ python/download-trade.py
@@ -112,7 +112,7 @@ def init_log(name, log_level=logging.INFO, use_file=True):
 
 def upload_binance_trade_files():
     symbols = get_all_symbols("t")
-    dates = pd.date_range(end=datetime.today().date() - timedelta(days=60), periods=1).to_pydatetime().tolist()
+    dates = pd.date_range(end=datetime.today().date() - timedelta(days=60), periods=35).to_pydatetime().tolist()
     dates = [date.strftime("%Y-%m-%d") for date in dates]
     start_date = datetime.utcnow().date() - timedelta(days=60)
     download_daily_trades("spot", symbols, len(symbols), dates, start_date, datetime.utcnow().date() - timedelta(days=28), None)
